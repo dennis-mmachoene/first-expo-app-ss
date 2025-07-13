f0203be40864e406f34661c71631a79060e3b952
Dennis Ramara <DRamara>
2025-07-13 20:52:11 +0200

    First expo app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,15 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
-  return (<Stack>
-<Stack.Screen name='(tabs)' options={{headerShown: false}} />
-
-  </Stack>);
+  return (
+  <>
+    <Stack>
+      <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
+    </Stack>
+    <StatusBar style="light"/>
+  </>
+  );
 }
 
 //A stack navigator is the foundation for navigating between different screens in an app. On Android, a stacked route animates on top of the current screen. On iOS, a stacked route animates from the right. Expo Router provides a Stack component to create a navigation stack to add new routes.
